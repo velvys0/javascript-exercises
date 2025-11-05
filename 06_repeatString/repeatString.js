abf106e6125070ecb3830164209b6aa145d14190
velvys0 <67652971+velvys0@users.noreply.github.com>
2025-11-05 15:48:42 +0100

    Complete exercices

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, number) {
+    let arr = [];
+    if (number < 0) {
+        return "ERROR"
+    } else {}
+    for (i = 1; i <= number; i++) {
+        arr.push(text);
+    }
+    arr = arr.join('');
 
+    return arr
 };
 
 // Do not edit below this line
