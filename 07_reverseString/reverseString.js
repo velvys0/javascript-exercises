a246ffe4fb9d262cd4f0930061a9b0c20bd410c7
velvys0 <67652971+velvys0@users.noreply.github.com>
2025-12-17 22:59:56 +0100

    Complete exercice

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let result = [];
+    result.push(text[0]);
+    for (n of text) {
+        result.push(n);
+        console.log(n);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remover) {
+for (let i = 0; i < array.length; i++) {
+    if (array[i] == remover) {
+        array.splice(i, 1);
+    }
+    
+}
+console.log(array);
+return array;
 };
 
 // Do not edit below this line
