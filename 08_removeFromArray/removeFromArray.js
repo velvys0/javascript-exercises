2b9bc45d6df2ba099e2fe0c303caa64c3affcbd0
velvys0 <67652971+velvys0@users.noreply.github.com>
2025-12-17 23:34:39 +0100

    Complete exercice

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,10 +1,16 @@
-const removeFromArray = function(array, remover) {
-for (let i = 0; i < array.length; i++) {
-    if (array[i] == remover) {
-        array.splice(i, 1);
+const removeFromArray = function(array, ...args) {
+
+
+
+for (let arg of args) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == arg && typeof(array[i]) == typeof(arg)) {
+            array.splice(i, 1);
+            i--;
+        }
     }
-    
 }
+
 console.log(array);
 return array;
 };
