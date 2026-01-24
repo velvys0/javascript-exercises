8860a75468286603ea1b89484c2af2b535547eed
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-24 16:18:54 +0100

    Complete multiply and power excercice

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,12 +10,16 @@ const sum = function(arr) {
 	return arr.reduce(((a,b) => a + b), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(((a,b) => a * b), 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1
+  for (i = 0; i < b; i++) {
+    result = result * a
+  }
+  return result
 };
 
 const factorial = function() {
