0d81f03fcbbee5fb0156603d3bb6a1e5ac48a84f
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-26 18:02:39 +0100

    Complete the first pass

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(toNum) {
+let result = [1]
+
+for (i = 1; i < toNum; i++) {
+    console.log('tste')
+    if (result.length === 1) {
+        result.push(1)
+        console.log(result)
+        continue
+    }
+    console.log(result.at(-1))
+    result.push(result.at(-1) + result.at(-2))
+    console.log(result)
+}
+return result.at(-1)
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
