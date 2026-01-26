f6a949860f79410e1297c99a449d9899678a37ae
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-26 16:48:04 +0100

    Modify system and complete second test

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,20 +1,21 @@
 const palindromes = function (input) {
     const forward = []
+    let backward = []
 
     for (letter in input) {
         console.log(input[letter])
         if (input[letter] === '!' || 
             input[letter] === ' ' ||
-            input[letter] === ','
+            input[letter] === ',' ||
+            input[letter] === '.'
         ) {
-            break
+            continue
         }
         forward.push(input[letter])
         console.log(forward)
     }
-
-    return forward === forward.reverse()
-
+    backward = [...forward].reverse()
+    return forward.join() === backward.join()
 };
 
 // Do not edit below this line
