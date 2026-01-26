1283eeed61a89cb9169874039463aa2fe86e0b20
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-26 18:53:40 +0100

    Correct two first iterations

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,19 +1,21 @@
 const fibonacci = function(toNum) {
-let result = [1]
 
-for (i = 1; i < toNum; i++) {
-    console.log('tste')
-    if (result.length === 1) {
-        result.push(1)
-        console.log(result)
-        continue
-    }
-    console.log(result.at(-1))
-    result.push(result.at(-1) + result.at(-2))
+let result = []
+
+for (i = 0; i < toNum; i++) {
+    a = 0
+    b = 0
+
+    result.at(-1) === undefined ? a = 1 : a = result.at(-1)
+    result.at(-2) === undefined ? b = 0 : b = result.at(-2)
+
+    result.push(a + b)
     console.log(result)
 }
-return result.at(-1)
 
+
+return result.at(-1)
 };
+
 // Do not edit below this line
 module.exports = fibonacci;
