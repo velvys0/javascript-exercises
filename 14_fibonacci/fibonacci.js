23626469d82df8d2b8ed9617f65c8f03104977e2
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-26 19:00:00 +0100

    Complete pass all test and remove console.log

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
 const fibonacci = function(toNum) {
 
+if (toNum < 0) {return 'OOPS'}
+
 let result = []
 
 for (i = 0; i < toNum; i++) {
@@ -10,11 +12,9 @@ for (i = 0; i < toNum; i++) {
     result.at(-2) === undefined ? b = 0 : b = result.at(-2)
 
     result.push(a + b)
-    console.log(result)
 }
 
-
-return result.at(-1)
+return result.at(-1) === undefined ? 0 : result.at(-1)
 };
 
 // Do not edit below this line
