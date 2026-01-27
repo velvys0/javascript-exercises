41d6332312261ae650cb70c62fe990f2d6cdf4f3
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-27 12:16:34 +0100

    Complete exercice

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
 
-};
+return arr.map(obj => obj.title)
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
