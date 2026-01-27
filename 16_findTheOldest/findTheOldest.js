1bc28274740ff15c1ae0facb531d950faf2e67ee
velvys0 <67652971+velvys0@users.noreply.github.com>
2026-01-27 12:47:23 +0100

    Complete the exercice

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+let sorted = people.sort((a, b) => {
+    if (b.yearOfDeath === undefined || a.yearOfDeath === undefined) {
+        let currYear = new Date().getFullYear() 
+        return (currYear - b.yearOfBirth) - (currYear - a.yearOfBirth)
+    }
+    return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+})
+return sorted[0]
 };
 
 // Do not edit below this line
